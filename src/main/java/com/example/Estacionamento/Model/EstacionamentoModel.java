f8c816b9a6275fe0878330c7e02e059b6bc9b7d5
Vdetofa <victordetofano13@gmail.com>
2026-04-13 23:13:56 -0300

    model Get e set iniciais

---
+++ src/main/java/com/example/Estacionamento/Model/EstacionamentoModel.java
@@ -0,0 +1,48 @@
+package com.example.Estacionamento.Model;
+
+import java.time.LocalDateTime;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class EstacionamentoModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+
+    String placa;
+    String modelo;
+    LocalDateTime dataHoraEntrada;
+    LocalDateTime dataHoraSaida;
+    double valorPago;
+
+    public EstacionamentoModel(){}
+
+    public EstacionamentoModel(String placa, String modelo, LocalDateTime dataHoraEntrada,LocalDateTime dataHoraSaida, double valorPago){
+        this.placa=placa;
+        this.modelo=modelo;
+        this.dataHoraEntrada=dataHoraEntrada;
+        this.dataHoraSaida=dataHoraSaida;
+        this.valorPago=valorPago;
+    }
+    
+    public long getID(){return id;}
+    public String getPlaca(){return placa;}
+    public LocalDateTime getDataHoraEntrada(){return dataHoraEntrada;}
+    public LocalDateTime getDataHoraSaida(){return dataHoraSaida;}
+    public double getValorPago(){return valorPago;}
+
+    public void setDataHoraEntrada(LocalDateTime dataHoraEntrada){
+        this.dataHoraEntrada=dataHoraEntrada;
+    }
+
+      public void setDataHoraSaida(LocalDateTime dataHoraSaida){
+        this.dataHoraSaida=dataHoraSaida;
+    }
+
+    
+}
